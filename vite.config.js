2d851f40a18b1c7ebeb7d35512719c073576a8f2
AquilesCancinos <aquilescancinos@gmail.com>
2025-09-22 18:31:10 -0300

    Arreglos

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
    plugins: [react()],
-   base: mode === "production" ? "/TPN3-LenguajesIV/" : "/", // <– nombre del repo, con misma mayúsc/minúscula
+   base: mode === "production" ? "/TPN3-LenguajesIV/" : "/", // ← con “s”
 }));
